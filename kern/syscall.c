6b1d23a4b266a91636676667d4b90477bfb5149a
Warren Toomey <wkt@tuhs.org>
2016-07-08 22:36:24 +1000

    Added ioctl.
    I've added the ioctl code from nyuichi/xv6.

--- kern/syscall.c
+++ kern/syscall.c
@@ -99,6 +99,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_lseek(void);
+extern int sys_ioctl(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -123,6 +124,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_lseek]   sys_lseek,
+[SYS_ioctl]   sys_ioctl,
 };
 
 void
