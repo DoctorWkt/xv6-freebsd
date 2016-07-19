89df29aea697862bdff9d23aac8a3290767d4260
Warren Toomey <wkt@tuhs.org>
2016-07-19 17:03:56 +1000

    /dev/dir with /dev/console
    I've moved the creation of the console
    special file into the /dev/directory.

--- cmd/old/init.c
+++ cmd/old/init.c
@@ -12,9 +12,9 @@ main(void)
 {
   int pid, wpid;
 
-  if(open("console", O_RDWR) < 0){
-    mknod("console", 1, 1);
-    open("console", O_RDWR);
+  if(open("/dev/console", O_RDWR) < 0){
+    mknod("/dev/console", 1, 1);
+    open("/dev/console", O_RDWR);
   }
   dup(0);  // stdout
   dup(0);  // stderr
