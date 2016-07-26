941e1d0b88f625464c65d457dec0cdcbb6522178
Warren Toomey <wkt@tuhs.org>
2016-07-26 10:58:58 +1000

    Fixing opendir: in progress
    ls isn't working, I have to fix opendir() to
    convert between xv6 and BSD dirent formats.
    In progress ...

--- lib/xv6/misc.c
+++ lib/xv6/misc.c
@@ -1,11 +1,19 @@
 #include <unistd.h>
 
-void sync(void) {
-  return;
-}
+void sync(void) { return; }
 
+unsigned int alarm(unsigned int seconds) { return(0); }
 
-unsigned int alarm(unsigned int seconds)
-{
-  return(0);
-}
+void logwtmp(void) { return; }
+
+void updwtmp(void) { return; }
+
+char *getlogin(void) { return("root"); }
+
+void openlog(void) { return; }
+
+void syslog(void) { return; }
+
+void closelog(void) { return; }
+
+void vsyslog(void) { return; }
--- lib/xv6/opendir.c
+++ lib/xv6/opendir.c
@@ -1,10 +1,16 @@
 /* Functions to read from a directory. 		Author: Warren Toomey */
 
+#include <stdio.h>
+#include <string.h>
 #include <limits.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#define dirent xv6dirent
+#include <xv6/types.h>
+#include <xv6/fs.h>
+#undef dirent
 
 int closedir(DIR *dirp)
 {
@@ -21,6 +27,7 @@ DIR *opendir(const char *dirname)
 
   if (dirname==NULL) return(NULL);
   dd_fd= open(dirname, O_RDONLY);
+printf("opendir on %s got %d\n", dirname, dd_fd);
   if (dd_fd==-1) return(NULL);
   dirp= (DIR *)malloc(sizeof(DIR));
   if (dirp==NULL) return(NULL);
@@ -31,13 +38,19 @@ DIR *opendir(const char *dirname)
 struct dirent *readdir(DIR *dirp)
 {
   struct dirent *d;
+  struct xv6dirent *x;
   int err;
 
   if (dirp==NULL) return(NULL);
   d= (struct dirent *)malloc(sizeof(struct dirent));
   if (d==NULL) return(NULL);
-  err= read(dirp->dd_fd, d, sizeof(struct dirent));
-  if (err < sizeof(struct dirent)) return(NULL);
+  err= read(dirp->dd_fd, &x, sizeof(struct xv6dirent));
+  if (err < sizeof(struct xv6dirent)) return(NULL);
+
+  // Copy over into BSD struct
+  strncpy(d->d_name, x->name, DIRSIZ);
+  d->d_namlen= strlen(d->d_name);
+  d->d_fileno= x->inum;
   return(d);
 }
 
